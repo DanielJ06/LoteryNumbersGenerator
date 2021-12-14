638c839f479cea2a5b935fc2807a78fc57fe75b9
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-12-14 16:12:10 -0300

    Finish the app

--- lotery/ViewController.swift
+++ lotery/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
+enum GameType: String {
+    case megasena = "Mega-Sena"
+    case quina = "Quina"
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbTitleType: UILabel!
+    @IBOutlet weak var scTypes: UISegmentedControl!
+    @IBOutlet var balls: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        showNums(for: .megasena)
+    }
+    
+    func showNums(for type: GameType) {
+        lbTitleType.text = type.rawValue
+        var game: [Int] = []
+        switch type {
+            case .megasena:
+                game = generateRandomNums(6, 60)
+                balls.last!.isHidden = false
+            case .quina:
+                game = generateRandomNums(5, 80)
+                balls.last!.isHidden = true
+        }
+        
+        for (index, num) in game.enumerated() {
+            balls[index].setTitle("\(num)", for: .normal)
+        }
     }
 
-
+    func generateRandomNums(_ amountOfNums: Int, _ maxNum: Int) -> [Int] {
+        var result: [Int] = []
+        while result.count < amountOfNums {
+            let random = Int(arc4random_uniform(UInt32(maxNum))+1)
+            if !result.contains(random) {
+                result.append(random)
+            }
+        }
+        return result.sorted()
+    }
+    
+    @IBAction func generateGame() {
+        switch scTypes.selectedSegmentIndex {
+            case 0:
+                showNums(for: .megasena)
+            default:
+                showNums(for: .quina)
+        }
+    }
+    
 }
 
